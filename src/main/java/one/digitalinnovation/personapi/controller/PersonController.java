c1ec5b49983a8ae97d90c6e4b58cde7d2b348bb3
Strubis <emersonlima910@gmail.com>
2021-08-07 22:58:22 -0300

    Adicionando o projeto Spring Boot

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Emerson
+ */
+@RestController
+@RequestMapping( "/api/v1/people" )
+public class PersonController {
+    
+    @GetMapping
+    private String test(){
+        return "Hello World!";
+    }
+}
